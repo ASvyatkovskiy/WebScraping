c7bc3c83420ebcb623bab39ee226c088d2cb6989
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-03-02 15:37:11 -0500

    Parse output to the following format: n.xlsx

---
+++ parse_output.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import glob
+from subprocess import Popen
+
+file_list = glob.glob("/tmp/*xlsx*")
+ifile = 1
+for f in file_list:
+    Popen("mv "+f+" "+str(ifile)+".xlsx",shell=True)
